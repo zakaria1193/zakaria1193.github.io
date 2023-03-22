d48e73be06eb80d3dca4c00e82300f57848183e5
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-03-22 02:02:10 +0100

    index.js: Add client side routing

--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 
+// Router
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import { createRoot } from 'react-dom/client';
+
 // Pages
 import Homepage from './components/homepage/Homepage';
+// import About from './components/about/About';
+// import Contact from './components/contact/Contact';
 
 // Theme
-import {AppContainer, theme} from './Theme'
+import { AppContainer, theme } from './Theme';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+const wrap = (Component) => {
+  return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppContainer>
-        <Homepage />
-      </ AppContainer>
+        <Component />
+      </AppContainer>
     </ThemeProvider>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: wrap(Homepage),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
