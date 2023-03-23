1d0a5b502b65335917f805d8ea75f3a33180a675
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-03-23 23:48:35 +0100

    Intro: Add line break

--- src/components/homepage/IntroBanner.js
+++ src/components/homepage/IntroBanner.js
@@ -20,7 +20,8 @@ const Intro = () => {
         </Typography>
         <Typography variant="body1" align="left">
           I'm Zak, a firmware engineer working on IoT devices at <Netatmo />. <br />
-          Here, I share my tips, thoughts, setup, and tools. Otherwise, this website is a playground for me to experiment with modern frontend web technologies.
+          Here, I share my tips, thoughts, setup, and tools. <br />
+          Otherwise, this website is a playground for me to experiment with modern frontend web technologies.
         </Typography>
       </Box>
       <Box component="img" src="/photo.jpg" alt="Zak" sx={{ maxWidth: '45%', height: 'auto' }} />
