262c731469c0d19217f261b89f41a7e1e048b81f
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-03-25 22:56:16 +0100

    Add Firebase

--- src/index.js
+++ src/index.js
@@ -45,3 +45,27 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+// --------------------------------------------------------------------------------------------
+// Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvUuNNbxr-Ubt0rCKiBob9EgvrZZYGb7Q",
+  authDomain: "personal-website-71663.firebaseapp.com",
+  projectId: "personal-website-71663",
+  storageBucket: "personal-website-71663.appspot.com",
+  messagingSenderId: "873028455797",
+  appId: "1:873028455797:web:a4208805c37a0345d7173c",
+  measurementId: "G-DR9GEPHH39"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
