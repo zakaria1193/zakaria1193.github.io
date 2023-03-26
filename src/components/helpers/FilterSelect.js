7da37f82d2f6fd57bb3d963bdecda81666e10dc3
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-03-26 18:52:14 +0200

    FilterSelect: Fix checkboxes

--- src/components/helpers/FilterSelect.js
+++ src/components/helpers/FilterSelect.js
@@ -4,19 +4,22 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
+import Checkbox from '@mui/material/Checkbox';
 
 const FilterSelect = ({ label, options, value, onChange }) => {
   return (
-    <FormControl fullWidth variant="outlined" sx={{ marginRight: 2, marginBottom: 2 }}>
+    <FormControl fullWidth variant="outlined" sx={{ margin: '1% 2%' }}>
       <InputLabel>{label}</InputLabel>
       <Select
         label={label}
         multiple
         value={value}
         onChange={onChange}
+        renderValue={(selected) => selected.join(', ')}
       >
         {options.map((option) => (
           <MenuItem key={option} value={option}>
+            <Checkbox checked={value.indexOf(option) > -1} />
             {option}
           </MenuItem>
         ))}
