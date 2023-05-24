699e790f6570983f306ec3be0d49b75176bc40dc
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-05-24 16:35:57 +0200

    Intro banner: Remove Typography around body to respect div in p nesting

--- src/components/homepage/IntroBanner.js
+++ src/components/homepage/IntroBanner.js
@@ -17,11 +17,9 @@ const Intro = () => {
         <Typography variant="h2" align="left" gutterBottom>
           Hi!
         </Typography>
-        <Typography variant="body1" align="left">
           I'm Zak, a firmware engineer working on IoT devices at <Netatmo />. <br />
           Here, I share my tips, thoughts, setup, and tools. <br />
           Otherwise, this website is a playground for me to experiment with modern frontend web technologies.
-        </Typography>
       </Box>
       <Box component="img" src="/photo.jpg" alt="Zak" sx={{ maxWidth: '45%', height: 'auto' }} />
     </Box>
