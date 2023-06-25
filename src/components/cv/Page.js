5c5c8a616a377fe08ee8e67a62ec474ed6df6eec
Zakaria Fadli <zakaria.fadli@legrand.com>
2023-06-25 02:45:53 +0200

    🎨 style(Page.js): add Education section title The Education section of the CV page was missing a title. This commit adds a Typography component with a variant of h3 and the text "Education" to provide a clear title for the section.

--- src/components/cv/Page.js
+++ src/components/cv/Page.js
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
 // Common components
 import Topbar from "../Topbar";
@@ -23,6 +24,9 @@ function Page() {
       <Topbar />
       <Stack spacing={2}>
         <Item>
+          <Typography variant="h3" gutterBottom>
+            Education
+          </Typography>
           <Education />
         </Item>
       </Stack>
